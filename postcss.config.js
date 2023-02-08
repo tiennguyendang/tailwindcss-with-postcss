c0f1fa6d4bc86595666d435a7c5baa6d361be440
Tien Nguyen Dang <tien.nguyendang@outlook.com>
2023-02-08 14:23:46 +0700

    feat: remove cssnano plugin in dev environment

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,20 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    cssnano: {
-      preset: "default",
-    },
+const basePlugins = {
+  tailwindcss: {},
+  autoprefixer: {},
+};
+const prodPlugins = {
+  cssnano: {
+    preset: "default",
   },
+};
+
+const isProduction = process.env.NODE_ENV === "production";
+
+module.exports = {
+  plugins: isProduction
+    ? {
+        ...basePlugins,
+        ...prodPlugins,
+      }
+    : basePlugins,
 };
